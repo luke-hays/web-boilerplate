2e92fa7ab7f1e734d1d438ebe2e1510dd60065f2
Midival <lukehays32@gmail.com>
2022-05-21 21:35:28 -0400

    style navbar

--- react/src/components/Navbar.tsx
+++ react/src/components/Navbar.tsx
@@ -1,12 +1,12 @@
 import { Link } from 'react-router-dom';
 
-import './Navbar.css';
-
 const Navbar = (): JSX.Element => {
   return (
-    <div id="NavbarContainer">
+    <div className="my-3 ml-20">
       <nav>
-        <Link to="/">Home</Link>
+        <Link to="/" className="mr-4">
+          Home
+        </Link>
         <Link to="about">About</Link>
       </nav>
     </div>
